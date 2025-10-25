ac8292c316267d7786b90a5082e4aebdd35b0ebe
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-10-25 10:48:35 +0530

    Time: 13 ms (16.67%) | Memory: 62.3 MB (18.52%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.ts
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.ts
@@ -1,44 +1,10 @@
 function differenceOfSum(nums: number[]): number {
     
-    let elementSum = 0, digitSum = 0;
-
-    for(let i = 0; i < nums.length; i++) {
-        elementSum += nums[i];
-        digitSum += sumOfAllDigits(nums[i]);
+    let eleSum = 0, digitSum = 0;
+    for(let num of nums) {
+        eleSum += num;
+        let digits = num.toString().split('').map( num => parseInt(num));
+        digitSum += digits.reduce(( acc, cur) => acc + cur);
     }
-
-    return absoluteDifference( elementSum, digitSum);
-    // return 0;
+    return Math.abs(eleSum - digitSum);
 };
-
-function sumOfAllDigits(num) {
-
-    let sum = 0;
-    while(num >= 10) {
-        sum = sum + (num % 10);
-        num = floor(num / 10);
-    }
-    sum += num;
-    return sum;
-
-}
-
-function absoluteDifference( x, y) {
-    
-    let num1 = 0, num2 = 0;
-    if(x > y) {
-        num1 = x;
-        num2 = y;
-    }else {
-        num1 = y;
-        num2 = x
-    }
-    return num1 - num2;
-
-}
-
-
-let floor = ( value ) => {
-    let newValue = value - (value % 1);
-    return newValue < 0 && value !== newValue ? newValue - 1 : newValue;
-}
