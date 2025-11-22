a3981773fd1dc42774dacdb2acbfec35d08b964b
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-11-22 23:29:13 +0530

    Time: 3 ms (34.03%), Space: 58.6 MB (5.43%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.ts
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -1,4 +1,22 @@
 function mySqrt(x: number): number {
     
-    return Math.floor(Math.sqrt(x));
+    // return Math.floor(Math.sqrt(x));
+    if(x < 2) return x;
+
+    let left = 1;
+    let right = Math.floor(x / 2);
+    let mid = Math.floor(( left + right ) / 2);
+    while(left <= right) {
+
+        let sq = mid * mid;
+        if(sq == x) return mid;
+        else if(sq > x) {
+            right = mid - 1;
+        }else {
+            left = mid + 1;
+        }
+
+        mid = Math.floor(( left + right ) / 2);
+    }
+    return mid;
 };
