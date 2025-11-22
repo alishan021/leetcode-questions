ff8eb8302a141081c5746eec361950914c7b76a9
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-11-22 22:47:41 +0530

    Time: 0 ms (100%), Space: 57.5 MB (61.63%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.ts
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.ts
@@ -1,13 +1,18 @@
 function differenceOfSums(n: number, m: number): number {
     
-    let num1 = 0, num2 = 0;
-    for(let i = 1; i <= n; i++) {
-        if(i % m === 0) {
-            num2 += i;
-        }else {
-            num1 += i;
-        }
-    }
+    // let num1 = 0, num2 = 0;
+    // for(let i = 1; i <= n; i++) {
+    //     if(i % m === 0) {
+    //         num2 += i;
+    //     }else {
+    //         num1 += i;
+    //     }
+    // }
 
-    return num1 - num2;
+    // return num1 - num2;
+    let total = (n * (n + 1)) / 2;
+    let divisible = Math.floor(n / m);
+    let num2 = m * ( divisible * ( divisible + 1 ) / 2);
+
+    return (total - num2) - num2;
 };
