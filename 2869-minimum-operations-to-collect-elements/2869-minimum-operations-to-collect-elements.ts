09ab262a2155e0d27123b8572c037108d1a78d8b
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-11-28 13:54:21 +0530

    Time: 0 ms (100%), Space: 57.8 MB (70%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.ts
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.ts
@@ -1,20 +1,10 @@
 function minOperations(nums: number[], k: number): number {
     
     let array = Array(nums.length).fill(0), index = k;
-    // for(let i = nums.length - 1; i >= 0; i--) {
-    //     if(nums[i] <= k && array[i] === 0) {
-    //         array[i] = 1;
-    //         index++;
-    //         if(index === k) return nums.length - i;
-    //     }
-    // }
-    // return nums.length;
-
     for(let i = nums.length - 1; i >= 0; i--) {
         if(array[nums[i]] === 0 && nums[i] <= k) {
             array[nums[i]] = 1;
             index--;
-            console.log(index, array, i)
             if(index === 0) return nums.length - i;
         }
     }
