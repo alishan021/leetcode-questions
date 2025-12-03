e9bbe87aa00e13b1d768e666a3e8788124c90dd3
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-12-03 13:15:31 +0530

    Time: 22 ms (5.95%), Space: 63.1 MB (22.3%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.ts
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.ts
@@ -1,4 +1,38 @@
 function reverseWords(s: string): string {
     
-    return s.split(' ').map( word => word.split('').reverse().join('')).join(' ');
+    // return s.split(' ').map( word => word.split('').reverse().join('')).join(' ');
+
+    // let result = "", word: string[] = [];
+    // for(let i = 0; i < s.length; i++) {
+    //     if(s[i] !== ' ') word.push(s[i]);
+    //     else {
+    //         result += reverse(word) + ' ';
+    //         word = [];
+    //     }
+    // }
+    // result += reverse(word);
+    // return result;
+
+     let result = "", word: string[] = [];
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] !== ' ') word.unshift(s[i]);
+        else {
+            result += word.join('') + ' ';
+            word = [];
+        }
+    }
+    result += word.join('');
+    return result;
 };
+
+// function reverse(word) {
+//     let left = 0, right = word.length - 1;
+//     console.log(word)
+//         while( left < right ) {
+//             console.log(word[left], word[right])
+//             [ word[left], word[right]] = [ word[right], word[left]];
+//             left++;
+//             right--;
+//         }
+//     return word.join('');
+// }
