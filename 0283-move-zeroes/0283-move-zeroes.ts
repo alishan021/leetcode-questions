1dfb5c6b72b0e78cf73a6dbf989dc47e7ad1d3e3
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-12-03 12:32:07 +0530

    Time: 133 ms (5.02%), Space: 61.8 MB (10.74%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+
+    let len = nums.length;
+    for(let i = 0; i < len - 1; i++) {
+        if(nums[i] === 0) {
+            let j = i;
+            while(j !== len - 1) {
+                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+                j++;
+            }
+            len--;
+            i--;
+        }
+    }
+    console.log(nums)
+};
