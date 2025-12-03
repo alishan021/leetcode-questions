89ab96a1034f37a059b31971f988acd38d50a712
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-12-03 12:38:48 +0530

    Time: 0 ms (100%), Space: 61.9 MB (10.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -3,16 +3,29 @@
  */
 function moveZeroes(nums: number[]): void {
 
-    let len = nums.length;
-    for(let i = 0; i < len - 1; i++) {
-        if(nums[i] === 0) {
-            let j = i;
-            while(j !== len - 1) {
-                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
-                j++;
-            }
-            len--;
-            i--;
+    // let len = nums.length;
+    // for(let i = 0; i < len - 1; i++) {
+    //     if(nums[i] === 0) {
+    //         let j = i;
+    //         while(j !== len - 1) {
+    //             [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+    //             j++;
+    //         }
+    //         len--;
+    //         i--;
+    //     }
+    // }
+
+    let pointer = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[pointer] = nums[i];
+            pointer++;
         }
+    }
+
+    while(pointer !== nums.length) {
+        nums[pointer] = 0;
+        pointer++;
     }
 };
