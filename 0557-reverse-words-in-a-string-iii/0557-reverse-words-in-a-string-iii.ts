1a42af523ad126b0eb4e1710d871c8d7c8d33bdc
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-12-03 12:41:43 +0530

    Time: 8 ms (83.64%), Space: 61.6 MB (85.87%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.ts
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.ts
@@ -1,3 +1,4 @@
 function reverseWords(s: string): string {
-    return s.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+    
+    return s.split(' ').map( word => word.split('').reverse().join('')).join(' ');
 };
