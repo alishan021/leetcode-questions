6de04857019fbbd86368306cb1c5d992c44be2e5
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-12-03 13:51:19 +0530

    Time: 2 ms (30.56%), Space: 56.7 MB (52.78%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.ts
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.ts
@@ -1,15 +1,24 @@
 function arraySign(nums: number[]): number {
     
-    let product = nums[0];
-    for(let i = 1; i < nums.length; i++) {
-        product = product * nums[i];
+    // let product = nums[0];
+    // for(let i = 1; i < nums.length; i++) {
+    //     product = product * nums[i];
+    // }
+
+    // if(product > 0) {
+    //     return 1;
+    // }else if (product < 0) {
+    //     return -1;
+    // }else {
+    //     return 0;
+    // }
+
+    let negatives = 0;
+    for(let num of nums) {
+        if(num === 0) return 0;
+        else if(num < 0) negatives++; 
     }
 
-    if(product > 0) {
-        return 1;
-    }else if (product < 0) {
-        return -1;
-    }else {
-        return 0;
-    }
+    if( negatives % 2 === 0) return 1;
+    else return -1;
 };
