456568d4e1112e4e0011bad9e387e853433a0694
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2025-12-06 13:18:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -0,0 +1,22 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+
+    for(let i = 0; i < flowerbed.length; i++) {
+        let adj = flowerbed[i];
+        // let badj = flowerbed[i - 1] || oppositeNum(flowerbed[i]);
+        // let aadj = flowerbed[i + 1] || oppositeNum(flowerbed[i]);
+        let badj = flowerbed[i - 1] || 0;
+        let aadj = flowerbed[i + 1] || 0;
+        console.log(adj, badj, aadj)
+        if(badj === adj && aadj === adj) {
+            flowerbed[i] = oppositeNum(flowerbed[i]);
+            n--;
+            console.log(flowerbed)
+        }
+    }
+    let result = n === 0 ? true : false;
+    return result;
+};
+
+function oppositeNum(num) {
+    return num = num === 1 ? 0 : 1;
+}
