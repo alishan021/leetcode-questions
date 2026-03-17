b953edbf3d4c8a4d32bfaeeea12c9b687efffd89
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2026-03-17 22:42:13 +0530

    Time: 1 ms (16.67%), Space: 55.9 MB (38.8%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
@@ -9,14 +9,14 @@ function search(nums: number[], target: number): number {
         if(nums[mid] === target) return mid;
 
         if(nums[left] <= nums[mid]) {
-            if(nums[left] <= target) {
+            if(target >= nums[left] && target <= nums[mid]) {
                 right = mid - 1;
             }else {
                 left = mid + 1;
             }
         }else {
             if(nums[right] >= nums[mid]) {
-                if(nums[right] >= target) {
+                if( target >= nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 }else {
                     right = mid - 1;
