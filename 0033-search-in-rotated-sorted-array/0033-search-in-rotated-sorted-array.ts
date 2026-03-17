7feeaf08849c4cb0eb107004a871de7069c2c974
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2026-03-17 16:05:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
@@ -1,3 +1,30 @@
 function search(nums: number[], target: number): number {
-    return nums.findIndex( num => num === target);
+    // return nums.findIndex( num => num === target);
+
+    let left = 0; 
+    let right = nums.length - 1;
+
+    while(left <= right) {
+        let mid = Math.floor((left + right)/ 2);
+        if(nums[mid] === target) return mid;
+
+        if(nums[left] <= nums[mid]) {
+            if(nums[left] <= target) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }else {
+            if(nums[right] >= nums[mid]) {
+                if(nums[right] >= target) {
+                    left = mid + 1;
+                }else {
+                    right = mid - 1;
+                }
+            }
+
+        }
+    }
+
+    return -1;
 };
