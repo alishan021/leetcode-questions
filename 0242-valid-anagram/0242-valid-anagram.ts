1ddc0a93130de1d555923881ac922d56956d8613
Muhammed Alishan P <141935276+alishan021@users.noreply.github.com>
2026-04-30 16:23:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -2,8 +2,20 @@ function isAnagram(s: string, t: string): boolean {
     
     if(s.length !== t.length) return false;
 
-    let ss = s.split('').sort().toString();
-    let tt = t.split('').sort().toString();
-    console.log(ss, tt)
-    return ss === tt ? true : false;
+    // let ss = s.split('').sort().toString();
+    // let tt = t.split('').sort().toString();
+    // console.log(ss, tt)
+    // return ss === tt ? true : false;
+
+    let map = new Map();
+    for(let i = 0; i < s.length; i++) {
+        map.set( s[i], (map.get(s[i]) || 0) + 1);
+        map.set( t[i], (map.get(t[i]) || 0) - 1);
+    }
+
+    for(let val of map.values()) {
+        if(val !== 0) return false;
+    }
+
+    return true;
 };
